86dc0eb7f30e9b02b1ae962dcaf0ea60f981548c
Kazuki Nishikawa <kazuki_nishikawa@voyagegroup.com>
2019-09-19 14:31:07 +0900

    [add] v3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ブランチ環境できるのかな〜 v2</p>
+        <p>ブランチ環境できるのかな〜 v3</p>
         <a
           className="App-link"
           href="https://reactjs.org"
